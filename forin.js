bd5ac1f57084541edbdcad46fbd5818e80bb07e8
Makoto Tanabe <xekid78@outlook.com>
2018-03-18 15:39:34 +0900

    Add files via upload

--- forin.js
+++ forin.js
@@ -1,4 +1,9 @@
-var team = ["佐藤", "鈴木", "田中", "岸田", "有森"];
-for (var key in team) {
-    console.log(team[key] + "さん");
-}
+(function () {
+    'use strict';
+
+    var team = ["佐藤", "鈴木", "田中", "岸田", "有森"];
+    for (var key in team) {
+        console.log(team[key] + "さん");
+    }
+
+})();
